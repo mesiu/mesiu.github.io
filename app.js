698e48d73a95c8a1a84ebd5d9473b06ed90a95df
mesiu <Melissa@Melissas-MacBook-Pro.local>
2017-01-23 23:35:24 +1100

    chore: Set up local server

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {
+    root: __dirname
+  });
+});
+
+app.listen(port, function() {
+  console.log('Server listening on http://localhost:%s', port);
+})
